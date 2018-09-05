5c15a732107f823848005b8e41a10b8fa68a377e
Jhovany Morales <jhovanymb.info@gmail.com>
2018-09-05 00:33:28 -0500

    pagination

---
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -0,0 +1,38 @@
+@if ($paginator->hasPages())
+    <ul class="pagination" role="navigation">
+        {{-- Previous Page Link --}}
+        @if ($paginator->onFirstPage())
+            <li class="disabled" aria-label="@lang('pagination.previous')"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+        @else
+            <li class="waves-effect" aria-label="@lang('pagination.previous')"><a href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')"><i class="material-icons">chevron_left</i></a></li>
+        @endif
+
+        {{-- Pagination Elements --}}
+        @foreach ($elements as $element)
+            {{-- "Three Dots" Separator --}}
+            @if (is_string($element))
+                <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+            @endif
+
+            {{-- Array Of Links --}}
+            @if (is_array($element))
+                @foreach ($element as $page => $url)
+                    @if ($page == $paginator->currentPage())
+                        <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                    @else
+                        <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
+                    @endif
+                @endforeach
+            @endif
+        @endforeach
+
+        {{-- Next Page Link --}}
+        @if ($paginator->hasMorePages())
+            <li class="page-item">
+                <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
+            </li>
+        @else
+            <li class="disabled" aria-label="@lang('pagination.previous')"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+        @endif
+    </ul>
+@endif
